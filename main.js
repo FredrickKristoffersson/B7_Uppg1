3556eb98c717ce05a81363e1ded9a2311ffc28d5
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-03-15 14:49:52 +0100

    Lagt in simpla frågeställningar, kommer tas bort senare

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const secretWord = "Leksak";
 const input = document.querySelector("input");
+const inputValue = document.querySelector("input").value;
+const submit_button = document.querySelector("button");
+const test = input.value;
+
+input.placeholder = `Dagens ord har ${secretWord.length} bokstäver`;
+
 input.addEventListener("input", () => {
     console.log(input.value);
     console.log(input.value.match(secretWord));
 })
+
+    submit_button.addEventListener("click", () => {
+        const test = input.value;
+        console.log(test);
+        console.log(test.length);
+        return test;
+    });
+
+    /* 
+    if :
+    indexvärde(bokstav) på input(sparad variabel) === secretWord indexvärde,
+    säg: bokstav stämmer
+    else if :
+    indexvärde(bokstav) på input(sparad variabel) = secret indexvärde längd=?
+    säg: bokstav finns, men inte denna plats
+    else : 
+    indexvärde(bokstav) = !INTE lika,
+    säg: bokstav finns inte i det hemliga ordet
+    */
--- main.test.js
+++ main.test.js
@@ -1,3 +1,14 @@
+import { describe, expect, test, xtest } from '@jest/globals';
+import secretWord from './main.js';
+import input from './main.js';
+
+describe('Variable length', () => {
+    test('Variable length equal to secret words length', () => {
+        const output = secretWord.length;
+        expect(output).toEqual(input.length);
+    });
+});
+// Presenteras hur många bokstäver det hemliga ordet har?
 // Sparas input-värde som variabel?
 // Matchar input-värdet med värdet för den sparade variabeln?
 // förändras input-variabel från string till array?
